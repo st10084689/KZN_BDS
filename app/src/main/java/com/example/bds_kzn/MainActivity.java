a56f46c6be2bc7146bb047a8b8a72eee2a95ec9f
st10084689 <83469722+st10084689@users.noreply.github.com>
2023-10-03 20:56:29 +0200

    added necessary activities and fragments

--- app/src/main/java/com/example/bds_kzn/MainActivity.java
+++ app/src/main/java/com/example/bds_kzn/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.bds_kzn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int HIDE_NAVIGATION_Bar_Delay = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
     }
+
+    public void init(){
+        //initializing the fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        HomeFragment home = new HomeFragment();
+        fragmentTransaction.add(R.id.content_fragment, home);
+        fragmentTransaction.commit();
+    }
+
+
+
 }
