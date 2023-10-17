0badff7c5cc1c52e57b6a15f6b9d14cdc4cd0362
st10084689 <83469722+st10084689@users.noreply.github.com>
2023-10-17 10:56:21 +0200

    now i need to add the animations and fix up the bugs in the application

---
+++ app/src/main/java/com/example/bds_kzn/DonationPersonalInfo.java
@@ -0,0 +1,14 @@
+package com.example.bds_kzn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonationPersonalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_personal_info);
+    }
+}
---
+++ app/src/main/java/com/example/bds_kzn/SliderAdapter.java
@@ -0,0 +1,48 @@
+package com.example.bds_kzn;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+    private Context context;
+    private int[] images = {R.drawable.event_details_place_holder, R.drawable.event_details_place_holder, R.drawable.event_details_place_holder};
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.event_details_slider_item, container, false);
+
+        ImageView imageView = itemView.findViewById(R.id.EventDetailsImage);
+        imageView.setImageResource(images[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
